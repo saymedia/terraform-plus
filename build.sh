61ea525510b491b271ab6ddd7625300ef9092b03
Martin Atkins <matkins@saymedia.com>
2015-10-29 18:49:04 -0700

    Set the version of T+ dists to include the plus version.
    This requires us to temporarily patch over the terraform/version.go file
    during the build.

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -xe
 
 export BASE_VERSION="v0.6.6"
+export PLUS_VERSION="0.3"
 export GOPATH="$PWD/gopath"
 export PATH="$GOPATH/bin:$PATH"
 export DISTDIR="$PWD/dist"
+export WORKDIR="$PWD"
 mkdir -p "$GOPATH"
 
 export GOX_MAIN_TEMPLATE="$DISTDIR/{{.OS}}/{{.Dir}}"
@@ -16,17 +18,39 @@ go get github.com/mitchellh/gox
 # We just assume the cross toolchains are already installed, since on Debian
 # there are deb packages for those.
 
-# Build Terraform itself
 go get -v github.com/hashicorp/terraform
-gox -arch="$GOX_ARCH" -os="$GOX_OS" -output="$GOX_MAIN_TEMPLATE" github.com/hashicorp/terraform
 
 cd $GOPATH/src/github.com/hashicorp/terraform
+git clean -dfx
+git reset --hard
 git fetch origin
 git checkout $BASE_VERSION
+
+# Adjust the version number to include our PLUS_VERSION
+BASE_VERSION_GIVEN=`perl -ne '/"(\d+\.\d+\.\d+)"/ && print $1' $GOPATH/src/github.com/hashicorp/terraform/terraform/version.go`
+FULL_PLUS_VERSION="$BASE_VERSION_GIVEN+$PLUS_VERSION"
+echo "--- Building TerraformPlus $FULL_PLUS_VERSION"
+echo "--- Based on Terraform commitish $BASE_VERSION"
+
+cat >$GOPATH/src/github.com/hashicorp/terraform/terraform/version.go <<EOF
+package terraform
+
+// Modified version.go for terraform-plus
+const Version = "$FULL_PLUS_VERSION"
+const VersionPrerelease = ""
+EOF
+
+# Build Terraform itself
+gox -arch="$GOX_ARCH" -os="$GOX_OS" -output="$GOX_MAIN_TEMPLATE" github.com/hashicorp/terraform
 
 # Build the standard plugins
 go get -v github.com/hashicorp/terraform/builtin/bins/...
 gox -arch="$GOX_ARCH" -os="$GOX_OS" -output="$GOX_PLUGIN_TEMPLATE" github.com/hashicorp/terraform/builtin/bins/...
+
+# Remove our overridden version.go so we don't get conflicts as we
+# switch branches.
+git clean -dfx
+git reset --hard
 
 # No we'll work on the plugins that are waiting to be merged into the main Terraform repo.
 git remote add apparentlymart git@github.com:apparentlymart/terraform.git || true
