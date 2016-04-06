5835d85d961e6cdfe49af58982284fd13d7752b0
Ross Hattori <git@exstasis.net>
2016-04-05 19:58:28 -0700

    release: Build off terraform 0.6.14

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -ex
 IFS=$' \n\t'
 
-export BASE_VERSION="v0.6.12"
+export BASE_VERSION="v0.6.14"
 export PLUS_VERSION="0.1"
 export PLUS_WORKING_BRANCH="terraform-plus-$BASE_VERSION-$PLUS_VERSION"
 export GOPATH="$PWD/gopath"
